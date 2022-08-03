64294596a6b1b53640d2898803a5129afd8a5a6b
alexandra spalato <alexaspalato@gmail.com>
2022-08-03 15:13:38 +0200

    connect shopify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,34 @@
+const fs = require("fs");
+require("dotenv").config({
+  path:
+    (fs.existsSync(`.env.${process.env.NODE_ENV}`) &&
+      `.env.${process.env.NODE_ENV}`) ||
+    ".env",
+});
 module.exports = {
   siteMetadata: {
     title: `gatsby-shopify-base`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-shopify',
-    options: {
-      "shopName": "shop-gatsby-test",
-      "accessToken": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: "gatsby-source-shopify",
+      options: {
+        storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
+        password: process.env.SHOPIFY_ADMIN_API_ACCESS_TOKEN,
+        shopifyConnections: ["collections"], // source product collections too
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };

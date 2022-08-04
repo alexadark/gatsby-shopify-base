ff5027faba0855480c011b63f1648e127829f976
Alexandra Spalato <alexaspalato@gmail.com>
2022-08-04 09:46:51 +0200

    tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,8 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-emotion`,
     {
       resolve: "gatsby-source-filesystem",
       options: {

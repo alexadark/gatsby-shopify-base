41dbf1345deb5621084af5938b5fe8cdc9a82b19
Alexandra Spalato <alexaspalato@gmail.com>
2022-08-04 09:18:52 +0200

    Product page data

--- src/pages/{shopifyProduct.handle}.js
+++ src/pages/{shopifyProduct.handle}.js
@@ -1,10 +1,57 @@
 import React from "react";
 import { graphql } from "gatsby";
 
-const ProductPage = () => {
-  return <div>ProductPage</div>;
+const ProductPage = ({ data }) => {
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
 };
 
 export default ProductPage;
 
-export const pageQuery = graphql``;
+export const pageQuery = graphql`
+  query ($handle: String!) {
+    shopifyProduct(handle: { eq: $handle }) {
+      title
+      description
+      hasOnlyDefaultVariant
+      hasOutOfStockVariants
+      id
+      options {
+        name
+        position
+        shopifyId
+        values
+      }
+      variants {
+        availableForSale
+        compareAtPrice
+        price
+        shopifyId
+        displayName
+        selectedOptions {
+          name
+          value
+        }
+        title
+        taxable
+        taxCode
+        media {
+          ... on ShopifyMediaImage {
+            id
+            image {
+              gatsbyImageData
+              altText
+            }
+          }
+        }
+      }
+      media {
+        ... on ShopifyMediaImage {
+          image {
+            gatsbyImageData
+            altText
+          }
+        }
+      }
+    }
+  }
+`;

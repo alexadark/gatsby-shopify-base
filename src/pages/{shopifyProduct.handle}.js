85cab289e3a6d1427a59498dc0283163a1012af0
Alexandra Spalato <alexaspalato@gmail.com>
2022-08-16 15:03:48 +0200

    options hooks and selector

--- src/pages/{shopifyProduct.handle}.js
+++ src/pages/{shopifyProduct.handle}.js
@@ -3,25 +3,18 @@ import { graphql } from "gatsby";
 import { Layout } from "~/components/Layout";
 import { Image } from "~/components/ui-components/Image";
 import { ButtonAddToCart, QuantityControls } from "~/components/shoppingCart";
+import { useOptions } from "~/hooks/useOptions";
+import { OptionSelector } from "~/components/product";
 
 const ProductPage = ({ data }) => {
   const { title, description, media, variants, options } = data.shopifyProduct;
   const [quantity, setQuantity] = useState(1);
 
-  const optionsObject = options.reduce((acc, option) => {
-    acc[option.name] = option.values[0];
-    return acc;
-  }, {});
+  const { selectedOptions, setSelectedOptions, selectedVariant } = useOptions(
+    options,
+    variants
+  );
 
-  const [selectedOptions, setSelectedOptions] = useState(optionsObject);
-
-  const optionsValues = Object.values(selectedOptions);
-
-  const selectedVariant = variants.find((variant) => {
-    const arrayFromTitle = variant.title.split("/").map((item) => item.trim());
-    return arrayFromTitle.every((item) => optionsValues.includes(item));
-  });
-  console.log("variant", selectedVariant);
   let { price, image } = selectedVariant;
   image = image ? image : media[0].image;
 
@@ -37,36 +30,11 @@ const ProductPage = ({ data }) => {
           <div className="mb-2 font-bold product__price">{price}eur</div>
           <div className="product__description">{description}</div>
           {options.length > 1 && (
-            <div className="flex gap-5 mt-5">
-              {options.map((option) => {
-                const { name, values, shopifyId } = option;
-                return (
-                  <div key={shopifyId}>
-                    <label htmlFor="options" className="block mb-1 font-bold">
-                      {name}
-                    </label>
-                    <form action="">
-                      <select
-                        name="options"
-                        id="options"
-                        onChange={(e) =>
-                          setSelectedOptions({
-                            ...selectedOptions,
-                            [name]: e.target.value,
-                          })
-                        }
-                      >
-                        {values.map((value, i) => (
-                          <option value={value} key={i}>
-                            {value}
-                          </option>
-                        ))}
-                      </select>
-                    </form>
-                  </div>
-                );
-              })}
-            </div>
+            <OptionSelector
+              options={options}
+              setSelectedOptions={setSelectedOptions}
+              selectedOptions={selectedOptions}
+            />
           )}
           <QuantityControls quantity={quantity} setQuantity={setQuantity} />
 

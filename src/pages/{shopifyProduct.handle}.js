c850327d7be079ad686a1b764bd0d008c5f8931a
Alexandra Spalato <alexaspalato@gmail.com>
2022-08-16 14:06:05 +0200

    selectedVariant

--- src/pages/{shopifyProduct.handle}.js
+++ src/pages/{shopifyProduct.handle}.js
@@ -15,27 +15,26 @@ const ProductPage = ({ data }) => {
 
   const [selectedOptions, setSelectedOptions] = useState(optionsObject);
 
-  const variantTitles = variants[0].title.split("/").map((item) => item.trim());
-
   const optionsValues = Object.values(selectedOptions);
-  const variant = variants.find((variant) => {
+
+  const selectedVariant = variants.find((variant) => {
     const arrayFromTitle = variant.title.split("/").map((item) => item.trim());
     return arrayFromTitle.every((item) => optionsValues.includes(item));
   });
-  console.log("variant", variant, variants, optionsValues);
+  console.log("variant", selectedVariant);
+  let { price, image } = selectedVariant;
+  image = image ? image : media[0].image;
 
   return (
     <Layout>
       <div className="container items-center gap-10 mx-auto my-10 product md:flex">
         <div className="flex-1 product__image">
           {" "}
-          <Image img={media[0].image} />{" "}
+          <Image img={image} />{" "}
         </div>
         <div className="flex-1 product__info">
           <h1 className="mb-2 font-bold">{title}</h1>
-          <div className="mb-2 font-bold product__price">
-            {variants[0].price}eur
-          </div>
+          <div className="mb-2 font-bold product__price">{price}eur</div>
           <div className="product__description">{description}</div>
           {options.length > 1 && (
             <div className="flex gap-5 mt-5">
@@ -43,7 +42,7 @@ const ProductPage = ({ data }) => {
                 const { name, values, shopifyId } = option;
                 return (
                   <div key={shopifyId}>
-                    <label htmlFor="options" className="block mb-1  font-bold">
+                    <label htmlFor="options" className="block mb-1 font-bold">
                       {name}
                     </label>
                     <form action="">
@@ -73,7 +72,7 @@ const ProductPage = ({ data }) => {
 
           <div className="mt-5 buttons">
             <ButtonAddToCart
-              variantId={variants[0].shopifyId}
+              variantId={selectedVariant.shopifyId}
               quantity={quantity}
               className="btn"
             />
